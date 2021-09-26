c5dd45aea926ae7869eb5256f9835c51426a93d5
Ranko <91413908+rankosnas@users.noreply.github.com>
2021-09-26 15:02:41 +0200

    Update main.cpp

--- blinky/main.cpp
+++ blinky/main.cpp
@@ -1,16 +1,17 @@
+// https://ww1.microchip.com/downloads/en/Appnotes/Getting-Started-with-GPIO-DS90003229B.pdf
 #include <Arduino.h>
+#include <avr/io.h>
+#include <avr/delay.h>
+
 
 void setup() {
   // put your setup code here, to run once:
-  PORTA_DIR |= (1<<PIN_PA3);      //Setup PIN_PA3 as OUTPUT
-  pinMode(PIN_PA3, OUTPUT);
+  PORTA_DIR |= PIN3_bm;      //Setup PIN_PA3 as OUTPUT bm BitMask 00000100
 }
 
 void loop() {
-  PORTA_OUT |= (1<<PIN_PA3);
-  digitalWrite(PIN_PA3,HIGH);
-  delay(100);
-  digitalWrite(PIN_PA3,LOW);
-  PORTA_OUT &= ~(1<<PIN_PA3);
-  delay(250);
+  PORTA_OUT |= PIN3_bm;     // Set PIN3 high
+  _delay_ms(150);
+  PORTA_OUT &= ~PIN3_bm;    // Set PIN low
+  _delay_ms(250);
   }
